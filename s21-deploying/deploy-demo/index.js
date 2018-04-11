dbac28c40df47de23ac14183f0aeb86ea70856d7
Tim Austin <tim@neenjaw.com>
2018-04-11 13:50:17 -0600

    deploy demo started

---
+++ s21-deploying/deploy-demo/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set('view-engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
